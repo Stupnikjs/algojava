239a7fa9c883e36132d232d63a45b00913622d82
Stupnikjs <n.boudier.ph@gmail.com>
2023-06-14 11:17:51 +0200

    graphs

---
+++ classes/graph/Graph.java
@@ -0,0 +1,22 @@
+package classes.graph;
+
+/* adjacency list
+* V: vertex edges
+* O(|V| + |E|)
+*
+* O(1) to add an edge
+* O(|E|) to remove an edge
+* O(|V| + |E|) to remove vertex
+ */
+
+
+/* adjacency matrix
+* O(|V|^2) to add vertex
+*
+*O(1) to add an edge
+*O(1) to remove an edge
+*O(|V|^2) to remove vertex
+ */
+public class Graph {
+
+}
---
+++ classes/graph/Main.java
@@ -0,0 +1,4 @@
+package classes.graph;
+
+public class Main {
+}
